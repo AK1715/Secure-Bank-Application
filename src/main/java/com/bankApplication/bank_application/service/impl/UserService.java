7daab4ead9a4699ff6d3b01000a90fd4a7d468f3
AK1715 <ns031077@gmail.com>
2026-01-31 14:45:04 +0530

    added method for balance check and credit amout

--- src/main/java/com/bankApplication/bank_application/service/impl/UserService.java
+++ src/main/java/com/bankApplication/bank_application/service/impl/UserService.java
@@ -1,9 +1,18 @@
 package com.bankApplication.bank_application.service.impl;
 
 import com.bankApplication.bank_application.dto.BankResponse;
+import com.bankApplication.bank_application.dto.CreditDebitRequest;
+import com.bankApplication.bank_application.dto.EnquiryRequest;
 import com.bankApplication.bank_application.dto.UserRequest;
 
 public interface UserService {
 
     BankResponse createAccount(UserRequest userRequest);
+
+    BankResponse balanceEnquiry(EnquiryRequest request);
+
+    String nameEnquiry(EnquiryRequest request);
+
+    BankResponse creditAccount(CreditDebitRequest request);
+
 }
