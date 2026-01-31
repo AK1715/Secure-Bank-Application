cb9cf1e74425ad77760fad8f79a71a34f599f76e
AK1715 <ns031077@gmail.com>
2026-01-31 14:43:06 +0530

    check Balance and credit amount

--- src/main/java/com/bankApplication/bank_application/controller/UserController.java
+++ src/main/java/com/bankApplication/bank_application/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.bankApplication.bank_application.controller;
 
 import com.bankApplication.bank_application.dto.BankResponse;
+import com.bankApplication.bank_application.dto.CreditDebitRequest;
+import com.bankApplication.bank_application.dto.EnquiryRequest;
 import com.bankApplication.bank_application.dto.UserRequest;
 import com.bankApplication.bank_application.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,4 +19,20 @@ public class UserController {
     public BankResponse createAccount(@RequestBody UserRequest userRequest){
         return userService.createAccount(userRequest);
     }
+
+    @GetMapping("/balanceEnquiry")
+    public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request){
+        return userService.balanceEnquiry(request);
+    }
+
+    @GetMapping("/nameEnquiry")
+    public String nameEnquiry(@RequestBody EnquiryRequest request){
+        return userService.nameEnquiry(request);
+    }
+
+    @PostMapping("/creditAccount")
+    public BankResponse creditAccount(@RequestBody CreditDebitRequest request){
+        return userService.creditAccount(request);
+    }
+
 }
