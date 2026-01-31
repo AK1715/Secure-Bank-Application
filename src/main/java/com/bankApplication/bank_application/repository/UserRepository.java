8c505b77db8e0a51f0e5a8f911c543ca3a69ceef
AK1715 <ns031077@gmail.com>
2026-01-31 14:44:16 +0530

    added new query for credit amout and balance check

--- src/main/java/com/bankApplication/bank_application/repository/UserRepository.java
+++ src/main/java/com/bankApplication/bank_application/repository/UserRepository.java
@@ -9,4 +9,8 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<@NonNull User, @NonNull Long> {
 
     Boolean existsByEmail(String email);
+
+    Boolean existsByAccountNumber(String accountNumber);
+
+    User findByAccountNumber(String accountNumber);
 }
