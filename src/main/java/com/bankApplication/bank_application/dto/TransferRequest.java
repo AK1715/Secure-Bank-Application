a0792179d7b3e1bde333ee8e1cf8cfddd07e0653
AK1715 <ns031077@gmail.com>
2026-02-02 18:53:42 +0530

    transfer DTO added

--- src/main/java/com/bankApplication/bank_application/dto/TransferRequest.java
+++ src/main/java/com/bankApplication/bank_application/dto/TransferRequest.java
@@ -1,4 +1,19 @@
 package com.bankApplication.bank_application.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class TransferRequest {
+
+    private String sourceAccountNumber;
+    private String destinationAccountNumber;
+    private BigDecimal amount;
 }
