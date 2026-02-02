8ced252dbc1dc884cc35228f76f5cdd8831dd2d6
AK1715 <ns031077@gmail.com>
2026-02-02 18:53:24 +0530

    added debit credit transfer

--- src/main/java/com/bankApplication/bank_application/controller/UserController.java
+++ src/main/java/com/bankApplication/bank_application/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.bankApplication.bank_application.controller;
 
-import com.bankApplication.bank_application.dto.BankResponse;
-import com.bankApplication.bank_application.dto.CreditDebitRequest;
-import com.bankApplication.bank_application.dto.EnquiryRequest;
-import com.bankApplication.bank_application.dto.UserRequest;
+import com.bankApplication.bank_application.dto.*;
 import com.bankApplication.bank_application.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,19 +17,29 @@ public class UserController {
         return userService.createAccount(userRequest);
     }
 
-    @GetMapping("/balanceEnquiry")
+    @GetMapping("/balance")
     public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request){
         return userService.balanceEnquiry(request);
     }
 
-    @GetMapping("/nameEnquiry")
+    @GetMapping("/name")
     public String nameEnquiry(@RequestBody EnquiryRequest request){
         return userService.nameEnquiry(request);
     }
 
-    @PostMapping("/creditAccount")
+    @PostMapping("/credit")
     public BankResponse creditAccount(@RequestBody CreditDebitRequest request){
         return userService.creditAccount(request);
+    }
+
+    @PostMapping("/debit")
+    public BankResponse debitAmount(@RequestBody CreditDebitRequest request){
+        return userService.debitAccount(request);
+    }
+
+    @PostMapping("/transfer")
+    public BankResponse transfer(@RequestBody TransferRequest request){
+        return userService.transfer(request);
     }
 
 }
