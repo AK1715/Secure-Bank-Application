a6aef7b23499cf92c107a7a9dc3cfe9d5dddac56
AK1715 <ns031077@gmail.com>
2026-02-03 18:53:30 +0530

    Transaction Coltroller

---
+++ src/main/java/com/bankApplication/bank_application/controller/TransactionController.java
@@ -0,0 +1,29 @@
+package com.bankApplication.bank_application.controller;
+
+import com.bankApplication.bank_application.entity.Transaction;
+import com.bankApplication.bank_application.service.impl.BankStatement;
+import com.itextpdf.text.DocumentException;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/bankStatement")
+@AllArgsConstructor
+public class TransactionController {
+
+    private BankStatement bankStatement;
+
+    @GetMapping
+    public List<Transaction> generateBankStatement(@RequestParam String accountNumber,
+                                                   @RequestParam String startDate,
+                                                   @RequestParam String endDate) throws DocumentException, FileNotFoundException {
+        return bankStatement.generateStatement(accountNumber, startDate, endDate);
+    }
+
+}
