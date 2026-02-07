9f70cc0ea45d33da1c57bf3e039f5b4a004e2e48
AK1715 <ns031077@gmail.com>
2026-02-07 10:37:41 +0530

    created new query

--- src/main/java/com/bankApplication/bank_application/repository/UserRepository.java
+++ src/main/java/com/bankApplication/bank_application/repository/UserRepository.java
@@ -5,10 +5,14 @@ import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<@NonNull User, @NonNull Long> {
 
     Boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 
     Boolean existsByAccountNumber(String accountNumber);
 
